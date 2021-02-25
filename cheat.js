1ff0ce8b5fdeb6332df6eb92b117fd37fda2974d
Kinetix <lawr3572@gmail.com>
2021-02-25 18:50:29 -0500

    Works!

---
+++ cheat.js
@@ -0,0 +1,43 @@
+let tries = 0;
+let worked = false;
+
+function retrieveAssignmentID() {
+    return new URL(document.URL).pathname.split('/')[2]
+}
+
+function xmlRequest() {
+    const XML = new XMLHttpRequest();
+    XML.open(('GET', `${'https://edpuzzle.com/api/v3/assignments/'}${getAssignment()}${''}`, true));
+    XML.onerror = (Filler) => {
+        worked = false;
+        tries++;
+        xmlRequest()
+    };
+    XML.onload = (Filler) => {
+        jsonData(XML.response)
+    };
+    XML.send()
+}
+
+function jsonData(Data) {
+    let i = 0;
+    let json = JSON.parse(Data);
+    //console.log(_0x503ax9);
+    json['medias'][0]['questions'].forEach((questions) => {
+        i++;
+        //console.log(_0x503ax10['body'].innterHTML)
+        questions['choices'].forEach((choices) => {
+            if(choices['isCorrect'] == true) {
+                var textFinal = choices['body'][0]['html'];
+                console.log(`Q${i} - ` + textFinal.replaceAll(/<[^>]*>/ig, ""));
+            }
+        })
+
+    })
+    alert('Answers are in the console. Open it via Inspect Element. - Kinetix');
+}
+
+setTimeout(() => {
+    console.clear();
+    xmlRequest()
+}, 1000)
